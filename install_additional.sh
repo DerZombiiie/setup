4dcb7cf8ed593784b5524f7d53a00915b3022ef7
Edzell <derz@elidragon.com>
2022-02-28 00:22:55 +0000

    Added nvm / node / insect & discord

--- install_additional.sh
+++ install_additional.sh
@@ -9,12 +9,36 @@ sudo apt install airshipper
 
 mkdir ~/Downloads/
 cd ~/Downloads
+
+# typora
 wget https://download.typora.io/linux/typora_0.11.18_amd64.deb -O typora.deb
-sudo apt install ./typora.deb
+sudo apt install ./typora.deb -y
+	# typora themes:
+	git clone https://github.com/elitistsnob/typora-cobalt-theme.git ~/.config/Typora/themes/typora-cobalt-theme
+	mv -r ~/.config/Typora/themes/typora-cobalt-theme/* ~/.config/Typora/themes/
 
-# typora themes:
-git clone https://github.com/elitistsnob/typora-cobalt-theme.git ~/.config/Typora/themes/typora-cobalt-theme
-mv -r ~/.config/Typora/themes/typora-cobalt-theme/* ~/.config/Typora/themes/
+# discord
+wget https://dl.discordapp.net/apps/linux/0.0.17/discord-0.0.17.deb -O discord.deb
+sudo apt install ./discord.deb -y
 
+# nvm
+cd ~
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# node
+nvm install node npm
+
+# insect
+npm i -g insect
+
+# micro
 cd /bin/
 curl -sL https://getmic.ro | sudo bash
+

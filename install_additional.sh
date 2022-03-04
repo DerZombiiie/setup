87330b64be9f1f4647d5a0255e0abf89a3ec27ad
Edzell <derz@elidragon.com>
2022-03-04 13:40:22 +0000

    die

--- install_additional.sh
+++ install_additional.sh
@@ -1,9 +1,10 @@
+
 #! /bin/bash
 # Install additional desktop programs
 sudo -E apt install sddm okular ark vlc firefox nextcloud-desktop nemo apt-transport-https flatpak binwalk ffmpeg adb fastboot default-jdk gimp i3lock-fancy ifupdown intel-gpu-tools kdenlive macchanger mc nfs-common nload pavucontrol python3-pip sound-juicer sl tmux torbrowser-launcher tty-clock traceroute xclip xinput-calibrator wget curl qbittorrent obs-studio krita xclip g++ make libc6-dev cmake libpng-dev libjpeg-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libzstd-dev libluajit-5.1-dev partclone
+sudo -E apt install oneko neofetch
 
 # design stuff
-
 sudo -E apt install freecad cura
 
 # airshipper

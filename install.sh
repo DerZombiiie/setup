ecec6bffa81a123cc781ad6b228653e77d742c2b
riley lutz <riley@e926.de>
2023-03-22 16:55:49 +0100

    remove apt support; cleanup

--- install.sh
+++ install.sh
@@ -3,108 +3,65 @@ function command_exists {
 	command -v $1 &> /dev/null
 }
 
-if [[ "$USER" != "root" ]]; then
-	if command_exists sudo; then
-		SUDO="sudo -E"
-	elif command_exists doas; then
-		SUDO="doas"
-		if ! ENV_TEST="test123" doas env | grep "ENV_TEST=test123" > /dev/null; then
-			echo "Please make sure doas keeps the environment for emerge (add keepenv to /etc/doas.conf)"
-			exit 1
-		fi
-	fi
+function need_command {
+	if ! command_exists $1
+	then
+		echo "required command $1 not found!"
+		exit 1
+	fi	
+}
+
+need_command pacman
+
+# ensure sudo
+if command_exists sudo; then
+	SUDO="sudo"
+elif command_exists doas; then
+	SUDO="doas"
 fi
 
-if command_exists apt; then
-	$SUDO add-apt-repository -y ppa:regolith-linux/stable	
-	$SUDO apt install -y \
-		i3-gaps \
-		i3status \
-		nemo \
-		feh \
-		picom \
-		bat \
-		lolcat \
-		cowsay \
-		fortune \
-		zsh \
-		lua5.3 \
-		git \
-		imagemagick \
-		kde-spectacle
-
-elif command_exists pacman; then
-	$SUDO pacman -S \
-		i3-gaps \
-		i3status \
-		feh \
-		picom \
-		bat \
-		lolcat \
-		cowsay \
-		fortune-mod \
-		zsh \
-		lua53 \
-		git \
-		imagemagick \
-		spectacle
-
-elif command_exists emerge; then
-	USE="
->=x11-libs/cairo-1.16.0-r5 X
->=x11-libs/libxkbcommon-1.3.1 X
->=x11-libs/pango-1.48.10 X
->=media-libs/freetype-2.11.0-r2 harfbuzz
->=sys-auth/pambase-20210201.1 elogind
->=media-libs/libglvnd-1.3.4
->=net-wireless/wpa_supplicant-2.9-r5 dbus
->=app-crypt/gcr-3.40.0 gtk
-" \
-	$SUDO emerge --ask --newuse \
-		x11-base/xorg-x11 \
-		x11-wm/i3-gaps \
-		x11-misc/dmenu \
-		x11-misc/i3lock \
-		x11-misc/i3status \
-		gnome-extra/nm-applet \
-		x11-misc/xss-lock \
-		media-gfx/feh \
-		x11-misc/picom \
-		sys-apps/bat \
-		games-misc/lolcat \
-		games-misc/cowsay \
-		games-misc/fortune-mod \
-		app-shells/zsh \
-		dev-lang/lua:5.3 \
-		dev-vcs/git \
-		media-gfx/imagemagick \
-		media-fonts/ubuntu-font-family \
-		media-fonts/hack
-
-	$SUDO ln -s /usr/local/bin/bat /usr/bin/batcat
-else
-	echo "Distro not supported"
+if [[ -n "$SUDO" ]]; then
+	echo "Neither sudo nor doas are supported on your system!"
 	exit 1
 fi
+
+$SUDO pacman -S \
+	i3-gaps \
+	i3status \
+	feh \
+	picom \
+	bat \
+	lolcat \
+	cowsay \
+	fortune-mod \
+	zsh \
+	lua53 \
+	git \
+	imagemagick \
+	spectacle
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k >> /dev/null
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | bash
 
-mkdir -p ~/bin/
-cp cat ~/bin/
+mkdir -p ~/.local/bin/
+ln -s ~/.local/bin/i3-wallpaper $PWD/i3-wallpaper
+chmod +x ~/.local/bin/i3-wallpaper
 
 mkdir -p ~/.config/i3/
-cp i3-config ~/.config/i3/config
+ln -s ~/.config/i3/config $PWD/i3-config
 
 mkdir -p ~/.config/kitty/
-cp kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/.config/kitty/kitty.conf $PWD/kitty.conf
 
 mkdir -p ~/.config/
-cp picom.conf ~/.config/picom.conf
+ln -s ~/.config/picom.conf $PWD/picom.conf
 
 mkdir -p ~/
-cp zshrc ~/.zshrc
+ln -s ~/.zshrc $PWD/zshrc
 
-wget https://i.imgur.com/BQx8EDo.jpg -O ~/wallpaper.png
+wget https://i.imgur.com/BQx8EDo.jpg -O ~/wallpaper.png.astolpho
+wget https://i.imgur.com/MrGY5EL.jpeg -O ~/wallpaper.png.xp
+~/.local/bin/i3-wallpaper set xp
+
--- install_additional.pacman.sh
+++ install_additional.pacman.sh
@@ -3,16 +3,26 @@ function command_exists {
 	command -v $1 &> /dev/null
 }
 
-if [[ "$USER" != "root" ]]; then
-	if command_exists sudo; then
-		SUDO="sudo -E"
-	elif command_exists doas; then
-		SUDO="doas"
-		if ! ENV_TEST="test123" doas env | grep "ENV_TEST=test123" > /dev/null; then
-			echo "Please make sure doas keeps the environment for emerge (add keepenv to /etc/doas.conf)"
-			exit 1
-		fi
-	fi
+function need_command {
+	if ! command_exists $1
+	then
+		echo "required command $1 not found!"
+		exit 1
+	fi	
+}
+
+need_command pacman
+
+# ensure sudo
+if command_exists sudo; then
+	SUDO="sudo"
+elif command_exists doas; then
+	SUDO="doas"
+fi
+
+if [[ -n "$SUDO" ]]; then
+	echo "Neither sudo nor doas are supported on your system!"
+	exit 1
 fi
 
 $SUDO pacman -S flatpak
@@ -23,38 +33,39 @@ $SUDO pacman -S oneko neofetch iperf3
 # design stuff
 $SUDO pacman -S freecad cura
 
+# aur-helper - PARU
+mkdir -p ~/src
+
+$SUDO pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git ~/src/paru
+cd ~/src/paru
+makepkg -si
+cd ~
+
 # other stuff
-$SUDO pamac build tty-clock-git xinput_calibrator oneko koneko-git
+paru -S tty-clock-git xinput_calibrator oneko koneko-git
 
 # i3 stuff
-$SUDO pamac build i3lock-fancy-rapid-git
+paru -S i3lock-fancy-rapid-git
 $SUDO pacman -S dmenu
 
-# STYLING
+# ich hab ich hab style und das geld... https://www.youtube.com/watch?v=XzAA5Qq9jzc
 $SUDO pacman -S lxappearance
 
 # java
 $SUDO pacman -S jdk8-openjdk jdk11-openjdk jdk11-openjdk
 
 # adb & fastboot
-pamac build android-sdk-platform-tools
+paru -S android-sdk-platform-tools
 
 # airshipper
-$SUDO pamac build airshipper
+paru -S build airshipper
 
 mkdir ~/Downloads/
 cd ~/Downloads
 
 # typora
-# the old way & garbage, because im a cheapskate and wont pay $15
-
-$SUDO pacman -S dpkg
-wget https://download.typora.io/linux/typora_0.11.18_amd64.deb -O typora.deb
-$SUDO dpkg -i ./typora.deb
-
-	# typora themes:
-	git clone https://github.com/elitistsnob/typora-cobalt-theme.git ~/.config/Typora/themes/typora-cobalt-theme
-	mv ~/.config/Typora/themes/typora-cobalt-theme/* ~/.config/Typora/themes/
+paru -S typora
 
 # discord
 $SUDO pacman -S discord
@@ -79,33 +90,15 @@ npm i -g insect
 # micro
 $SUDO pacman -S micro
 
-# rpm to arch
-$SUDO pacman -S ruby rpm-tools
-
-cd /bin
-$SUDO wget https://gist.githubusercontent.com/zinovyev/0ad6bd54ac5a9f7b709607a8151a0334/raw/481f891b73b1144ceb9531b1488591bd851d796d/rpmtoarch -O rpm_to_arch
-$SUDO chmod +x rpm_to_arch
-
-# skype
-cd ~/Downloads
-wget https://go.skype.com/skypeforlinux-64.rpm -O skype.rpm
-
-cd skypeforlinux
-makepkg -s
-sudo pacman -U *.pkg.tar.zst
+paru -S skypeforlinux-stable-bin
 
 # basic free vpn packages:
-# protonvpn:
-$SUDO pamac build protonvpn-cli
-
-# ovpn3
-$SUDO pamac build openvpn3
-
-# windscribe
-$SUDO pamac build windscribe-cli
-$SUDO pamac build windscribe-v2-bin
+paru -S protonvpn-cli \
+	openvpn3 \
+	windscribe-cli windscribe-v2-bin \
+	mullvad-cli mullvad
 
 # cbonsai
-$SUDO pamac build cbonsai-git
+paru -S cbonsai-git
 
 
